31e42a3cb506d94e10e7c87daef024bde22ea788
Fabiojr7 <fabiojrsilveira09@gmail.com>
2024-05-30 20:46:18 -0300

    feat: Chess layer and printing the board
    Reviewed-by: Fabiojr7 <fabiojrsilveira09@gmail.com>
    Refs: #4

--- src/main/java/org/project/Main.java
+++ src/main/java/org/project/Main.java
@@ -2,14 +2,15 @@ package org.project;
 
 import org.project.boardgame.Board;
 import org.project.boardgame.Position;
+import org.project.chess.ChessMatch;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
-        Position position1 = new Position(3, 5);
+        ChessMatch chessMatch = new ChessMatch();
 
-        Board board = new Board(8,8);
+        UI.printBoard(chessMatch.getPieces());
     }
 }
---
+++ src/main/java/org/project/chess/Color.java
@@ -0,0 +1,7 @@
+package org.project.chess;
+
+public enum Color {
+
+    BLACK,
+    WHITE;
+}
