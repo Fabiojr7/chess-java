f3d42b9037997281de6cc12ab711bcc741da83fe
Fabiojr7 <fabiojrsilveira09@gmail.com>
2024-08-14 09:23:34 -0300

    feat: placing pieces on the board
    Reviewed-by: Fabiojr7 <fabiojrsilveira09@gmail.com>
    Refs: #5

---
+++ src/main/java/org/project/chess/pieces/King.java
@@ -0,0 +1,17 @@
+package org.project.chess.pieces;
+
+import org.project.boardgame.Board;
+import org.project.chess.ChessPiece;
+import org.project.chess.Color;
+
+public class King extends ChessPiece {
+
+    public King(Board board, Color color) {
+        super(board, color);
+    }
+
+    @Override
+    public String toString() {
+        return "K";
+    }
+}
