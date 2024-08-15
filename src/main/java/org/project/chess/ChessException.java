f26cfed6d9f7ac6392201ca0cae6b025dcc4adeb
Fabiojr7 <fabiojrsilveira09@gmail.com>
2024-08-15 13:23:00 -0300

    feat: ChessException and ChessPosition.
    Reviewed-by: Fabiojr7 <fabiojrsilveira09@gmail.com>
    Refs: #7

---
+++ src/main/java/org/project/chess/ChessException.java
@@ -0,0 +1,9 @@
+package org.project.chess;
+
+public class ChessException extends RuntimeException{
+    private static final long serialVersionUID = 1L;
+
+    public ChessException(String message) {
+        super(message);
+    }
+}
